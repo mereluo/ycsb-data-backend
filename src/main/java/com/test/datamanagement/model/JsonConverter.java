9f5e80cd2cf96170a9fc7796499d512534fb0f9e
Simon Zhang <zhangshi0512@gmail.com>
2023-11-07 11:45:41 -0800

    Update JsonConverter and TimeSeries

--- src/main/java/com/test/datamanagement/model/JsonConverter.java
+++ src/main/java/com/test/datamanagement/model/JsonConverter.java
@@ -1,20 +1,38 @@
 package com.test.datamanagement.model;
 
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
 import com.google.gson.Gson;
-import jakarta.persistence.AttributeConverter;
-import jakarta.persistence.Converter;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.List;
 
-@Converter(autoApply = true)
-public class JsonConverter implements AttributeConverter<TimeSeries, String> {
-  private final static Gson GSON = new Gson();
+@Converter
+public class JsonConverter implements AttributeConverter<List<TimeSeries>, String> {
+    private final Gson gson = new GsonBuilder().create();
 
-  @Override
-  public String convertToDatabaseColumn(TimeSeries mjo) {
-    return GSON.toJson(mjo);
-  }
+    @Override
+    public String convertToDatabaseColumn(List<TimeSeries> attribute) {
+        if (attribute == null) {
+            return null;
+        }
+        Type type = new TypeToken<List<TimeSeries>>() {}.getType();
+        return gson.toJson(attribute, type);
+    }
 
-  @Override
-  public TimeSeries convertToEntityAttribute(String dbData) {
-    return GSON.fromJson(dbData, TimeSeries.class);
-  }
+    @Override
+    public List<TimeSeries> convertToEntityAttribute(String dbData) {
+        if (dbData == null || dbData.isEmpty()) {
+            return null;
+        }
+        Type type = new TypeToken<List<TimeSeries>>() {}.getType();
+        try {
+            return gson.fromJson(dbData, type);
+        } catch (JsonSyntaxException e) {
+            throw new IllegalArgumentException("Error deserializing JSON", e);
+        }
+    }
 }
+
